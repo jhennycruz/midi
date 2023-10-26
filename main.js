c5c1d399ec7e10c6c557139e815111f27d849e5b
jhenny <106121071+jhennycruz@users.noreply.github.com>
2023-10-26 14:22:35 -0300

    comentario

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-alert('ola mundo')
+alert('ola mundo');
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('elemento não encontrado');
+        console.long('Elemento não encontrado ou seletor inválido');
+    
+    }
+}
+
+const listaDeteclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTlecas[contador];
+    
+}
