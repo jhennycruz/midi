98788c900e420a4d36e94385b9b0ef02302791cd
jhenny <106121071+jhennycruz@users.noreply.github.com>
2023-11-16 14:03:23 -0300

    a

--- main.js
+++ main.js
@@ -18,5 +18,25 @@ const listaDeteclas = document.querySelectorAll('.tecla');
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTlecas[contador];
+    const instrumentos = tecla.classList[1];
+    const idaudio = '#som_${instrumento}'; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        teclaclassList.remove('ativa');
+    }
+
+}
     
 }
